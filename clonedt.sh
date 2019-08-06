c5f041815a0c377125ac7de07323842f39d7363a
MD Raza <raza231198@gmail.com>
2019-08-06 13:49:26 +0530

    script to clone dt
    Signed-off-by: MD Raza <raza231198@gmail.com>

---
+++ clonedt.sh
@@ -0,0 +1,4 @@
+git clone -b lineage-15.1 https://github.com/noobdeb/android_device_asus_msm8937-common device/asus/msm8937-common
+git clone -b lineage-15.1 https://github.com/noobdeb/android_device_asus_X00P device/asus/X00P
+git clone -b lineage-15.1 https://github.com/noobdeb/proprietary_vendor_asus vendor/asus
+git clone -b lineage-15.1 https://github.com/raza231198/android_kernel_asus_msm8937 kernel/asus/msm8937
